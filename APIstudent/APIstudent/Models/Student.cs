5bfef2f04f3ef6018cb1c9409e76fb308fa07524
Ricardo Gonzales Delgado <a2018118765@estudiantes.upsa.edu.bo>
2020-08-25 00:22:23 -0400

    student controller

---
+++ APIstudent/APIstudent/Models/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace APIstudent.Models
+{
+    public class Student
+    {
+     [key]
+     [Required]
+     [Range(1,9999)]
+     public int StudentID { get; set; }
+
+     [StringLength(30)]
+     [Required(ErrorMessage = "you must enter the field {0}")]
+     public string LastName { get; set; }
+
+     [StringLength(30, ErrorMessage = "the field {0} contain betwen {2} and {1} characters", MinimumLength = 2)]
+     [Required(ErrorMessage = "you must enter the field {0}")]
+     public string FirstName { get; set; }
+
+     [DataType(DataType.Date)]
+     [DisplayFormat(DataFormatString ="{0:dd/MM/YYYY}",ApplyFormatInEditMode =true)]
+     public DateTime EnrollmentDate { get; set; }
+
+    }
+}
---
+++ APIstudent/APIstudent/Models/keyAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace APIstudent.Models
+{
+    internal class keyAttribute : Attribute
+    {
+    }
+}
